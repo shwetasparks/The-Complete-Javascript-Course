36430e32e0ce2861a02c851a972d1113e087dfc3
Shweta <shwetakoffficial@gmail.com>
2024-03-26 13:15:56 +0530

    strict mode

---
+++ Section-3/StrictMode.js
@@ -0,0 +1,10 @@
+"use strict";
+let hasDriversLisence = false;
+const passTest = true;
+
+if (passTest) hasDriversLisence = true;
+if (hasDriversLisence) console.log("I can drive");
+
+const interface='Audio'
+
+const private=534;
