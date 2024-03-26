b0d2ac978c844234a83b1025d838042db72aad6d
Shweta <shwetakoffficial@gmail.com>
2024-03-26 18:29:47 +0530

    function calling function

---
+++ Section-3/FunctioncallingFuntion.js
@@ -0,0 +1,12 @@
+const cutPieces=function(fruit){
+    return fruit*4;
+}
+
+const fruitProcessor= function (apples,oranges){
+    const applePiece=cutPieces(apples)
+    const orangesPieces=cutPieces(oranges);
+    const juice=`Juice with ${applePiece} piece of apple and ${orangesPieces} piece of oranges`
+    return juice;
+}
+
+console.log(fruitProcessor(2,3));
