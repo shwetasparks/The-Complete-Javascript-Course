085c2f12a46f31712503e68a538f825d25278a53
Shweta <shwetakoffficial@gmail.com>
2024-03-28 15:36:12 +0530

    basic dom

--- Section7/index.js
+++ Section7/index.js
@@ -1,3 +1,32 @@
- let element=document.getElementById("myElement");
- element.innerHTML('hello world')
- 
+// let element = document.querySelector("#element");
+// // let para = document.querySelector(".para");
+// // console.log(para);
+// //  let paraEle=document.getElementsByClassName("para");
+// //  console.log(paraEle);
+
+
+
+//5. querySelectorAll
+// let element = document.querySelectorAll(".element");
+// console.log(element);
+
+//6.Traversing Dom
+//1. parent node
+// const heading=document.querySelector('.heading');
+// const parents=heading.parentNode;
+// console.log(parents);
+
+//2.childNodes
+
+// const parent=document.querySelector('.parent');
+// const children=parent.childNodes;
+// console.log(children);
+
+//nextElementSibling
+const heading=document.querySelector('.heading');
+console.log(heading.nextElementSibling);
+
+
+
+
+//manupulation
